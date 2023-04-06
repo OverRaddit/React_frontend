3da0eb2c3a9feda9812e37a76052e64e33bdfd57
Overraddit <kenwooshim@gmail.com>
2023-04-06 19:57:52 +0900

    Feat: basic chat feature

--- src/App.js
+++ src/App.js
@@ -12,9 +12,18 @@ import ProfilePage from './profile/ProfilePage.tsx';
 
 const socket = io("ws://localhost:8000");
 
+const exampleChatHistory = [
+  // 'Hello, how are you?',
+  // 'I am doing well, thanks for asking.',
+  // 'What have you been up to lately?',
+  // 'Not much, just working on some coding projects.',
+];
+
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [lastPong, setLastPong] = useState(null);
+  const [chatHistory, setChatHistory] = useState(exampleChatHistory);
+  const [currentChat, setCurrentChat] = useState('');
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -29,31 +38,67 @@ function App() {
       setLastPong(new Date().toISOString());
     });
 
+    socket.on('welcome', () => {
+      setChatHistory([...chatHistory, 'someone join the chatRoom!']);
+      console.log('someone join the chatRoom@');
+    })
+
+    socket.on('chat', (chat) => {
+      setChatHistory([...chatHistory, chat]);
+    })
+
     return () => {
       socket.off('connect');
       socket.off('disconnect');
       socket.off('pong');
     };
-  }, []);
+  }, [chatHistory]);
 
   const sendPing = () => {
     socket.emit('ping');
   }
+
+  const sendJoin = () => {
+    socket.emit('join', 'gshim');
+  }
+
+  const sendHi = () => {
+    console.log('sendHi');
+    socket.emit('chat', "hi");
+  }
+
+  const handleChatChange = (e) => {
+    setCurrentChat(e.target.value);
+  };
+
+  const handleChatSubmit = (e) => {
+    e.preventDefault();
+    if (currentChat.trim() !== '') {
+      setChatHistory([...chatHistory, 'You: ' + currentChat]);
+      socket.emit('chat', currentChat);
+      setCurrentChat('');
+    }
+  };
 
   return (
     <Router>
       <div className="App">
         <div className="App-content">
           <Navigation />
+          <div>
+            <h1>HI</h1>
+            <p>Connected: { '' + isConnected }</p>
+            <p>Last pong: { lastPong || '-' }</p>
+            <button onClick={sendPing}>Send ping</button>
+            <button onClick={sendJoin}>Join</button>
+            <button onClick={sendHi}>chat hi</button>
+          </div>
           <Routes>
-            <Route path="/a" element={<XPage />} />
+            <Route path="/a" element={<XPage chatHistory={chatHistory} onChatSubmit={handleChatSubmit} onChatChange={handleChatChange} currentChat={currentChat} />} />
             <Route path="/b" element={<YPage />} />
             <Route path="/c" element={<ZPage />} />
             <Route path="/profile" element={<ProfilePage />} />
           </Routes>
-          {/* <p>Connected: { '' + isConnected }</p>
-          <p>Last pong: { lastPong || '-' }</p>
-          <button onClick={sendPing}>Send ping</button> */}
         </div>
       </div>
     </Router>
--- src/pages/XPage.js
+++ src/pages/XPage.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import './XPage.css'
 
-const XPage = () => {
-  const [chatHistory, setChatHistory] = useState([]);
-  const [currentChat, setCurrentChat] = useState('');
+const XPage = ({ chatHistory, onChatSubmit, onChatChange, currentChat }) => {
+  // const [chatHistory, setChatHistory] = useState(chats);
+  // const [currentChat, setCurrentChat] = useState('');
 
-  const handleChatChange = (e) => {
-    setCurrentChat(e.target.value);
-  };
+  const chatHistoryRef = useRef(null); // new2
 
-  const handleChatSubmit = (e) => {
-    e.preventDefault();
-    if (currentChat.trim() !== '') {
-      setChatHistory([...chatHistory, currentChat]);
-      setCurrentChat('');
-    }
-  };
+  // const handleChatChange = (e) => {
+  //   setCurrentChat(e.target.value);
+  // };
+
+  // const handleChatSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (currentChat.trim() !== '') {
+  //     setChatHistory([...chatHistory, currentChat]);
+  //     setCurrentChat('');
+  //   }
+  // };
+
+  useEffect(() => {
+    const chatHistoryBox = chatHistoryRef.current;
+    const gap = chatHistoryBox.scrollHeight - chatHistoryBox.scrollTop
+    console.log('gap: ', gap);
+    if (gap < 230)
+      chatHistoryBox.scrollTop = chatHistoryBox.scrollHeight;
+  }, [chatHistory]);
 
   return (
     <div className="x-page">
@@ -23,20 +34,26 @@ const XPage = () => {
         <button>Expand Button</button>
       </div>
       <div className="x-page-bottom">
-        <div className="chat-history-box">
+        <div
+          className="chat-history-box"
+          ref={chatHistoryRef}
+          // onScroll={handleScroll}
+        >
           <ul>
             {chatHistory.map((chat, index) => (
               <li key={index}>{chat}</li>
             ))}
           </ul>
         </div>
-        <form onSubmit={handleChatSubmit}>
+        <form onSubmit={onChatSubmit}>
           <input
+            className='chat-input-box'
             type="text"
             value={currentChat}
-            onChange={handleChatChange}
+            onChange={onChatChange}
             placeholder="Type your chat here"
           />
+          <button type="submit">Send</button>
         </form>
       </div>
     </div>
