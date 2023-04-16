001f6b21bbf063adc781c9bfdd7624de8c8c6a39
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2023-04-16 17:45:45 +0900

    기본 프로필 수정

--- src/profile/ProfilePage.tsx
+++ src/profile/ProfilePage.tsx
@@ -9,7 +9,7 @@ interface ProfilePageProps {
   friend?: boolean;
   // 최근 전적 정보를 받을 props
   recentRecords?: {
-    opponentPicture: string;
+    opponentPicture?: string;
     opponentNickname: string;
     isWin: boolean;
     myScore: number;
@@ -24,26 +24,26 @@ interface ProfilePageProps {
 }
 
 const ProfilePage: React.FC<ProfilePageProps> = ({
-  profilePicture = defaultProfilePicture,
+  profilePicture,
   nickname = 'Unknown',
   friend = false,
   recentRecords = [
     {
-      opponentPicture: 'https://example.com/opponent1.jpg',
+      opponentPicture: "",
       opponentNickname: 'Player1',
       isWin: true,
       myScore: 5,
       opponentScore: 2,
     },
     {
-      opponentPicture: 'https://example.com/opponent2.jpg',
+      opponentPicture: "",
       opponentNickname: 'Player2',
       isWin: false,
       myScore: 1,
       opponentScore: 5,
     },
     {
-      opponentPicture: 'https://example.com/opponent3.jpg',
+      opponentPicture: "",
       opponentNickname: 'Player3',
       isWin: true,
       myScore: 5,
@@ -55,6 +55,8 @@ const ProfilePage: React.FC<ProfilePageProps> = ({
   onAddFriend,
   onRemoveFriend,
 }) => {
+  const displayProfilePicture = profilePicture || defaultProfilePicture;
+
   const friendButton = friend ? (
     <button className="friend-delete-button" onClick={onRemoveFriend}>
       친구 삭제
@@ -71,24 +73,27 @@ const ProfilePage: React.FC<ProfilePageProps> = ({
     <div className="profile-page">
       <div className="profile-info">
         <div className="profile-picture-wrapper">
-          <img className="profile-picture" src={profilePicture} alt="프로필 사진" />
+          <img className="profile-picture" src={displayProfilePicture} alt="프로필 사진" />
         </div>
         <div className="profile-nickname">{nickname}</div>
         <div className="friend-button-wrapper">{friendButton}</div>
       </div>
       <div className="profile-recent-record">
-        {recentRecords.map((record, index) => (
+        {recentRecords.map((record, index) => {
+          const displayOpponentPicture = record.opponentPicture || defaultProfilePicture;
+          return(
           <div key={index} className="recent-record-item">
             <div className="recent-opponent-info">
               <div className="recent-opponent-picture-wrapper">
-                <img className="recent-opponent-picture" src={record.opponentPicture} alt="최근 상대방 사진" />
+                <img className="recent-opponent-picture" src={displayOpponentPicture} alt="최근 상대방 사진" />
               </div>
               <div className="recent-opponent-nickname">{record.opponentNickname}</div>
             </div>
             <div className={`recent-result ${record.isWin ? 'win' : 'lose'}`}>{record.isWin ? '승리' : '패배'}</div>
             <div className="recent-score">{`${record.myScore} : ${record.opponentScore}`}</div>
           </div>
-        ))}
+          )
+          })}
       </div>
     </div>
   );
