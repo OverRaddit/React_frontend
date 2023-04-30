e3cac3d1da45086d2a66c594078128c99d380725
hena <somnium1108@gmail.com>
2023-04-30 17:07:27 +0900

    roll back

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -6,7 +6,6 @@ import io from 'socket.io-client';
 
 const DefaultPage = ({socket}) => {
   const navigate = useNavigate();
-  const [inputText, setInputText] = useState('');
 
   useEffect(() => {
     socket.on('matchingcomplete', () => {
@@ -15,19 +14,9 @@ const DefaultPage = ({socket}) => {
     });
   }, [navigate, socket]);
 
-  const handleClick = (e) => {
-	setInputText(e.target.value);
-  }
-
-  const sendInvite = () => {
-	console.log(inputText + "님에게 초대를 보냈습니다.");
-  }
-
   return (
     <div>
       <h1>Default Main Page!</h1>
-	  <input id='inputTextField' type="text" onChange={handleClick} placeholder='Input username'></input>
-	  <button onClick={sendInvite}>초대하기</button>
     </div>
   );
 };
