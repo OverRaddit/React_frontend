45ec6e7c911f3884f87675e8d9c56beebdbdb9cd
hena <somnium1108@gmail.com>
2023-05-10 20:56:42 +0900

    [Refactor]
    - 1. add [pos, setPos]'s useState value: score
    - 2. relocate isLeft event
    - 3. add key handler conversion
    - 4. add drawNet function
    - 5. add drawText function

--- src/game/GamePage.tsx
+++ src/game/GamePage.tsx
@@ -13,8 +13,8 @@ function Game() {
   const [gameOver, setGameOver] = useState(false);
   const [isInGame, setIsInGame] = useState(true);
   const [playerId, setPlayerId] = useState(0);
-  const [pos1, setPos1] = useState({ x: 0, y: 0, width: 0, height: 0 });
-  const [pos2, setPos2] = useState({ x: 0, y: 0, width: 0, height: 0 });
+  const [pos1, setPos1] = useState({ x: 0, y: 0, width: 0, height: 0, score:0 });
+  const [pos2, setPos2] = useState({ x: 0, y: 0, width: 0, height: 0, score:0 });
   const [ball, setBall] = useState({ x: 0, y: 0, radius: 0 });
   const [keyDown, setKeyDown] = useState(false);
   const [gameMode, setGameMode] = useState(0);
@@ -62,17 +62,7 @@ function Game() {
 
   const ballBlinkRate = 50;
 
-  mySocket?.gameSocket.on('isLeft', (num: string) => {
-    const number = parseInt(num);
-    setPlayerId(number);
-    console.log("my number", num);
-  });
-
-  mySocket?.gameSocket.on('render', (pos1: any, pos2: any, ball: any) => {
-    setPos1(pos1);
-    setPos2(pos2);
-    setBall(ball);
-  });
+  
 
   useLayoutEffect(() => {
     if (!gameData) {
@@ -88,13 +78,13 @@ function Game() {
   }, []);
 
   useEffect(() => {
-    // const windowKeyDownHandler = (e: KeyboardEvent) => {
-    //   handleKeyDown(e as unknown as React.KeyboardEvent<HTMLDivElement>);
-    // };
+    const windowKeyDownHandler = (e: KeyboardEvent) => {
+      handleKeyDown(e as unknown as React.KeyboardEvent<HTMLDivElement>);
+    };
   
-    // const windowKeyUpHandler = (e: KeyboardEvent) => {
-    //   handleKeyUp(e as unknown as React.KeyboardEvent<HTMLDivElement>);
-    // };
+    const windowKeyUpHandler = (e: KeyboardEvent) => {
+      handleKeyUp(e as unknown as React.KeyboardEvent<HTMLDivElement>);
+    };
   
     window.addEventListener('keydown', windowKeyDownHandler);
     window.addEventListener('keyup', windowKeyUpHandler);
@@ -106,8 +96,18 @@ function Game() {
   }, [playerId]);
 
   useEffect(() => {
-    // window.addEventListener('keydown', handleKeyDown as (e: React.KeyboardEvent<HTMLDivElement>) => void);
-    // window.addEventListener('keyup', handleKeyUp as (e: React.KeyboardEvent<HTMLDivElement>) => void);
+    mySocket?.gameSocket.on('isLeft', (num: number) => {
+      setPlayerId(num);
+      console.log("my number", num);
+    });
+  
+    mySocket?.gameSocket.on('render', (pos1: any, pos2: any, ball: any) => {
+      setPos1(pos1);
+      setPos2(pos2);
+      setBall(ball);
+    });
+
+
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
@@ -125,32 +125,48 @@ function Game() {
       ctx!.closePath();
       ctx!.fill();
     }
-    function drawCircle_extension(gameMode: number) {
-        if (gameMode === 1) {
-          if (Math.floor(ball.x / ballBlinkRate) % 2 === 0) {
-            drawCircle(ball.x, ball.y, ball.radius, 'WHITE');
-          } else {
-            drawCircle(ball.x, ball.y, ball.radius, 'BLACK');
-          }
-        } else {
+
+    function drawCircle_extension(gameMode: number)
+    {
+      if (gameMode === 1) {
+        if (Math.floor(ball.x / ballBlinkRate) % 2 === 0) {
           drawCircle(ball.x, ball.y, ball.radius, 'WHITE');
+        } else {
+          drawCircle(ball.x, ball.y, ball.radius, 'BLACK');
         }
+      } else {
+        drawCircle(ball.x, ball.y, ball.radius, 'WHITE');
       }
+    }
   
-      function render() {
-        drawRect(0, 0, canvasMaxWidth, canvasMaxHeight, 'BLACK');
-        drawRect(pos1.x, pos1.y, pos1.width, pos1.height, 'WHITE');
-        drawRect(pos2.x, pos2.y, pos2.width, pos2.height, 'WHITE');
-        drawRect(net.x, net.y, net.width, net.height, net.color);
-        drawCircle_extension(gameMode);
-      }
+    function drawNet() {
+      for (let i = 0; i <= canvas.height; i += 15)
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+    }
+
+    function drawText(text: number, x: number, y: number, color: string) {
+      ctx!.fillStyle = color;
+      ctx!.font = "35px fantasy";
+      ctx!.fillText(text.toString(), x, y);
+    }
+    function render() {
+      drawRect(0, 0, canvasMaxWidth, canvasMaxHeight, 'BLACK');
+      drawRect(pos1.x, pos1.y, pos1.width, pos1.height, 'WHITE');
+      drawRect(pos2.x, pos2.y, pos2.width, pos2.height, 'WHITE');
+
+      // draw the net
+      drawNet();
+
+      // draw score
+      drawText(pos1.score, canvas.width / 4, canvas.height / 5, "WHITE");
+      drawText(pos2.score, 3 * canvas.width / 4, canvas.height / 5, "WHITE");
+
+      drawCircle_extension(gameMode);
+    }
+
+    render();
+  }, [pos1, pos2, ball]);
   
-      render();
-    }, [pos1, pos2, ball]);
-  
-  
-  
-  // window.addEventListener('keyup', handleKeyUp);
   return (
     <div
       className="Game"
